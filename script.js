8597f5aef3174502063163e5b3bccb5d254f8cbf
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-06-09 17:25:11 +0200

    Commit 09/06/23 5pm
    Commit 09/06/23 5pm
     - Rehauled the nav to use JS instead of pure CSS
     - Changed button colours

--- script.js
+++ script.js
@@ -3,3 +3,34 @@ window.onbeforeunload = () => {
     form.reset();
   }
 }
+
+document.getElementById("navMenu").addEventListener("click", navShowMenu);
+
+function navShowMenu() {
+  document.getElementById("nav").style.transform = "scale(1, 1)";
+  document.getElementById("aboutMe").style.opacity = "1";
+  document.getElementById("projects").style.opacity = "1";
+  document.getElementById("contact").style.opacity = "1";
+  document.getElementById("navMenu").style.opacity = "0";
+  document.getElementById("navClose").style.transform = "scale(1, 1)";
+  document.getElementById("navClose").style.opacity = "1";
+}
+
+document.getElementById("navClose").addEventListener("click", navCloseMenu);
+
+function navCloseMenu() {
+  document.getElementById("nav").style.transform = "scale(1, 0)";
+  document.getElementById("navClose").style.opacity = "0";
+  document.getElementById("navClose").style.transform = "scale(1, 0)";
+  document.getElementById("navMenu").style.opacity = "1";
+}
+
+let widthMatch = window.matchMedia("(min-width: 1200px)");
+widthMatch.addEventListener('change', function(media) {
+    if (media.matches) {        
+      document.getElementById("nav").style.transform = "scale(1, 1)";
+    }
+    else {      
+      document.getElementById("nav").style.transform = "scale(1, 0)";
+    }
+});
