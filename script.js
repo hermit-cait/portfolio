61f4419535101dcb5219c24316fd84443b50bdae
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-06-09 17:37:41 +0200

    Fixed mobile nav by changing duplicate id's

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ window.onbeforeunload = () => {
 }
 
 document.getElementById("navMenu").addEventListener("click", navShowMenu);
+document.getElementById("navMenu").addEventListener("keydown", navShowMenu);
 
 function navShowMenu() {
   document.getElementById("nav").style.transform = "scale(1, 1)";
-  document.getElementById("aboutMe").style.opacity = "1";
-  document.getElementById("projects").style.opacity = "1";
-  document.getElementById("contact").style.opacity = "1";
+  document.getElementById("aboutLink").style.opacity = "1";
+  document.getElementById("projectsLink").style.opacity = "1";
+  document.getElementById("contactLink").style.opacity = "1";
   document.getElementById("navMenu").style.opacity = "0";
   document.getElementById("navClose").style.transform = "scale(1, 1)";
   document.getElementById("navClose").style.opacity = "1";
 }
 
 document.getElementById("navClose").addEventListener("click", navCloseMenu);
+document.getElementById("navClose").addEventListener("keydown", navCloseMenu);
 
 function navCloseMenu() {
   document.getElementById("nav").style.transform = "scale(1, 0)";
@@ -25,7 +27,8 @@ function navCloseMenu() {
   document.getElementById("navMenu").style.opacity = "1";
 }
 
-let widthMatch = window.matchMedia("(min-width: 1200px)");
+var widthMatch = window.matchMedia("(min-width: 1200px)");
+
 widthMatch.addEventListener('change', function(media) {
     if (media.matches) {        
       document.getElementById("nav").style.transform = "scale(1, 1)";
